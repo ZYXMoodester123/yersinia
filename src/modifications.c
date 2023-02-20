9da91e6e5863cea7c5943d75be9aa821955ba78b
ZYXMoodester123 <ryan.nathan.mudie@gmail.com>
2023-02-20 18:22:24 +0000

    menu

---
+++ src/modifications.c
@@ -0,0 +1,15 @@
+#include <modifications.h>
+
+void menu()
+{
+	int choice;
+
+	printf("Choose options:\n");
+	printf("1 - ARP Flooding\n");
+	printf("0 - Close Menu\n");
+
+	if (choice == 0)
+	{
+		return 0;
+	}
+}
