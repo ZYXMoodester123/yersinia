2beacc178ab82183edea6e73eec92969aa156114
ZYXMoodester123 <87921948+ZYXMoodester123@users.noreply.github.com>
2023-03-27 12:58:53 +0100

    Attempt 2 Signal

--- src/parser.c
+++ src/parser.c
@@ -95,11 +95,29 @@
 static u_int8_t valid_chars[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
 
 
+#include <signal.h>
+
 /*
  * Initial command line arguments parser.
  * Return -1 on error. Return 0 if Ok.
  * Use global protocol_defs
  */
+
+ void INThandler(int);
+
+ void INThandler(int sig)
+ {
+   char  c;
+   signal(sig, SIG_IGN);
+   printf("OUCH, did you hit Ctrl-C?\n"
+         "Do you really want to quit? [y/n] ");
+   c = getchar();
+   if (c == 'y' || c == 'Y')
+         exit(0);
+   else
+         signal(SIGINT, INThandler);
+   getchar(); // Get new line character
+ }
 
 void arpflooder()
 {
@@ -132,6 +150,7 @@ void arpflooder()
 
    // Flood the switch with packets
    printf("Flooding switch...\n");
+   signal(SIGINT, INThandler);
    while (1) {
       // Generate a random source MAC address
       for (i = 0; i < ETH_ALEN; i++) {
